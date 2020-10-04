63cbafc9ac990d6c9d3e177e04b2b71b5fa744d7
Marcus Price <marcusprice88@gmail.com>
2020-10-03 19:24:08 -0700

    added slug functionality

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,8 +1,17 @@
-'use strict';
+const slugify = require('slugify');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        beforeCreate(model) {
+            model.slug = slugify(model.title);
+        },
+        beforeUpdate(params, model) {
+            model.slug = slugify(model.title);
+        }
+      },
+};
